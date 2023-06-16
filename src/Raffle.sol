aa19dd22885a58891fa841e03de363da792c6c62
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-16 12:59:48 +0200

    update contract✅

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,6 +2,9 @@
 
 pragma solidity ^0.8.18;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+
 /**@title A sample Raffle Contract
  * @notice This contract is for creating a sample raffle contract
  * @dev This implements the Chainlink VRF Version 2
@@ -9,17 +12,31 @@ pragma solidity ^0.8.18;
 contract Raffle {
     error Raffle__NotEnoughEthSent();
 
+    /**State Variable */
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private constant NUM_WORDS = 1;
+
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
+
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
 
     /** Event */
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator,bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit) {
         i_entranceFee = entranceFee;
         i_interval = interval;
+        i_vrfCoordinator = VRFCoordinatorV2Interface (vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
         s_lastTimeStamp = block.timestamp;
     }
 
@@ -35,6 +52,14 @@ contract Raffle {
         if (block.timestamp - s_lastTimeStamp < i_interval) {
             revert();
         }
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATION,
+            i_callbackGasLimit,
+            NUM_WORDS 
+        
+        );
     }
 
     function getEntranceFee() public view returns (uint256) {
