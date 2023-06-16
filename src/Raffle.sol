8725ed556f67f74ffe54200ff6ed2bc7029e649f
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-16 11:00:38 +0200

    update contract✅

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -7,15 +7,25 @@ pragma solidity ^0.8.18;
  * @dev This implements the Chainlink VRF Version 2
  */
 contract Raffle {
-    uint256 private entranceFee;
-    function enterRaffle() public payable {
-        
+    error Raffle__NotEnoughEthSent();
+
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
     }
 
-    function pickWinner() public {
-        
+    function enterRaffle() external payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
+        s_players.push(payable(msg.sender));
     }
-     
-        
-    
+
+    function pickWinner() public {}
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
 }
