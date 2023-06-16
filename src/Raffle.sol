86fec19f5500d30835852449458cee7af949c94d
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-16 10:41:25 +0200

    first commit✨

---
+++ src/Raffle.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**@title A sample Raffle Contract
+ * @notice This contract is for creating a sample raffle contract
+ * @dev This implements the Chainlink VRF Version 2
+ */
+contract Raffle {
+    constructor() {
+        
+    }
+}
