6ff665d433dce15ec0b06694bfdba33d9b4255b6
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-18 14:24:55 +0200

    update contract✅

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 
 import {Script} from "forge-std/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
 
 pragma solidity ^0.8.18;
 
 contract DeployRaffle is Script {
-    
+    function run() external (Raffle)
+
 }
