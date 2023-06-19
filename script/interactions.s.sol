94b8283c4f1566212c78f70c5442f02e555d7a30
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-19 14:13:22 +0200

    update contracts✅

--- script/interactions.s.sol
+++ script/interactions.s.sol
@@ -9,7 +9,8 @@ import {VRFCoordinatorV2Mock} from "../test/mocks/VRFCoordinatorV2Mock.sol";
 contract CreateSubscription is Script {
     function createSubsciptionUsingConfig() internal returns (uint64) {
         HelperConfig helperConfig = new HelperConfig();
-        (, , address vrfCoordinator, , , ) = helperConfig.activeNetworkConfig();
+        (, , address vrfCoordinator, , , , ) = helperConfig
+            .activeNetworkConfig();
         return createSubscription(vrfCoordinator);
     }
 
@@ -18,7 +19,8 @@ contract CreateSubscription is Script {
     ) public returns (uint64) {
         console.log("Creating subscription on ChainId", block.chainid);
         vm.startBroadcast();
-        uint64 subId = VRFCoordinatorV2Mock(vrfCoordinator).createSubscription();
+        uint64 subId = VRFCoordinatorV2Mock(vrfCoordinator)
+            .createSubscription();
 
         vm.stopBroadcast();
         console.log("Your sub Id is:", subId);
@@ -28,5 +30,26 @@ contract CreateSubscription is Script {
 
     function run() external returns (uint64) {
         return createSubsciptionUsingConfig();
+    }
+}
+
+contract FundSubscription is Script {
+    uint96 public constant FUN_AMOUNT = 3 ether;
+
+    function FundSubscriptionConfig() public {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            ,
+            ,
+            address vrfCoordinator,
+            ,
+            uint64 subscriptionId,
+            ,
+            address link
+        ) = helperConfig.activeNetworkConfig();
+    }
+
+    function fun() external {
+        fundSubscriptionUsingConfig();
     }
 }
